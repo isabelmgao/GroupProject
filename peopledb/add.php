e06e1dc10fe5eeaa83147b99cf204ab8a516a001
isabelmgao <imgao@umich.edu>
2015-12-02 12:54:17 -0500

    Need to figure out bug
    Warning: Cannot modify header information - headers already sent by
    (output started at
    /Applications/MAMP/htdocs/howdy/GroupProject/peopledb/add.php:13) in
    /Applications/MAMP/htdocs/howdy/GroupProject/peopledb/add.php on line 37

--- peopledb/add.php
+++ peopledb/add.php
@@ -9,31 +9,32 @@ if ( isset($_SESSION['error']) ) {
   echo('<p style="color: red;">'.htmlentities($_SESSION['error'])."</p>\n");
   unset($_SESSION['error']);
 }
+if ( isset($_SESSION['success']) ) {
+  echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
+  unset($_SESSION['success']);
+}
 if(isset($_POST['cancel'])){
   header( 'Location: index.php' ) ;
   return;
 }
 if (isset($_POST['first_name']) && isset($_POST['last_name']) &&
-    isset($_POST['email']) && isset($_POST['headline']) &&
-    isset($_POST['summary'])){
-if ( strlen($_POST['first_name'])<1 || strlen($_POST['last_name'])<1 || strlen($_POST['email'])<1
-    || strlen($_POST['headline'])<1 || strlen($_POST['summary'])<1) {
+    isset($_POST['department'])){
+if ( strlen($_POST['first_name'])<1 || strlen($_POST['last_name'])<1 ||
+     strlen($_POST['department'])<1) {
       $_SESSION['error'] = "All fields are required";
       header("Location: add.php");
       return;
     }
   $stmt = $pdo->prepare('INSERT INTO Profile
-        (user_id, first_name, last_name, email, headline, summary) VALUES ( :uid, :fn, :ln, :email, :hl, :sum)');
+        (user_id, first_name, last_name, department) VALUES ( :uid, :fn, :ln, :depar)');
     $stmt->execute(array(
         ':uid' => $_SESSION['user_id'],
         ':fn' => $_POST['first_name'],
         ':ln' => $_POST['last_name'],
-        ':email' => $_POST['email'],
-        ':hl' => $_POST['headline'],
-        ':sum' => $_POST['summary'])
+        ':depar' => $_POST['department'])
     );
     $_SESSION['success']="Record added";
-    header("Location: index.php");
+    header("Location: add.php");
     return;
 
 }
@@ -52,13 +53,13 @@ if ( strlen($_POST['first_name'])<1 || strlen($_POST['last_name'])<1 || strlen($
 <p>Last Name:
 <input type="text" name="last_name" size="60"/></p>
 <p>Department:<br/>
-<input type="text" name="website" size="80"/></p>
-</form>
+<input type="text" name="department" size="80"/></p>
+<!-- </form> -->
 <!-- Upload photo -->
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<!-- <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload (if applicable):
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Upload Image" name="submit"> -->
 <p>
 <input type="submit" value="Add">
 <input type="submit" name="cancel" value="Cancel">
