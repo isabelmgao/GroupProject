7249ab31fda0aa60057342333475edd9a5651872
isabelmgao <imgao@umich.edu>
2015-12-03 16:06:24 -0500

    Fully functional working update

--- peopledb/add.php
+++ peopledb/add.php
@@ -14,8 +14,7 @@ if(isset($_POST['cancel'])){
 if (isset($_POST['first_name']) && isset($_POST['last_name']) &&
     isset($_POST['department'])){
 
-    var_dump($_FILES);
-    die();
+
     if ( strlen($_POST['first_name'])<1 || strlen($_POST['last_name'])<1 ||
        strlen($_POST['department'])<1) {
          $_SESSION['error'] = "All fields are required";
@@ -23,25 +22,56 @@ if (isset($_POST['first_name']) && isset($_POST['last_name']) &&
           return;
     }
 
+    // Do all that image upload.php
+    $target_dir = "member_img/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+    // Check if image file is a actual image or fake image
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check === false) {
+        $_SESSION['error']= "File is not an image.";
+        header("Location: add.php");
+        return;
+    }
+
+    //Check file extension
+    // if($imageFileType != '.jpg' || $imageFileType != '.jpeg' ||
+    //    $imageFileType != '.png'){
+    //      $_SESSION['error']="Image file not recognized. Please upload a jpeg, jpg, or png file";
+    //     //  header("Location: add.php");
+    //    }
+
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        $_SESSION['error']="Sorry, your file is too large.";
+        header("Location: add.php");
+        return;
+    }
+
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file) === false) {
+        $_SESSION['error']="Sorry, there was an error uploading your file.";
+        header("Location: add.php");
+        return;
+    }
+
+    //text fields
     $stmt = $pdo->prepare('INSERT INTO Profile
-        (user_id, first_name, last_name, department) VALUES ( :uid, :fn, :ln, :depar)');
+        (user_id, first_name, last_name, department, filename) VALUES ( :uid, :fn, :ln, :depar, :file )');
     $stmt->execute(array(
         ':uid' => $_SESSION['user_id'],
         ':fn' => $_POST['first_name'],
         ':ln' => $_POST['last_name'],
-        ':depar' => $_POST['department'])
-    );
+        ':depar' => $_POST['department'],
+        ':file' => $target_file));
+
     $_SESSION['success']="Record added";
     header("Location: add.php");
     return;
-
-}
-
-//put the temp data back in after image upload form (upload.php) runs
-// $stmt = $pdo->prepare("SELECT * FROM Profile where profile_id = :xyz");
-// $stmt->execute(array(":xyz" => $_GET['id']));  //where is the get method receiving auto_id from? coming from id GET parameter sent from view.php
-// $row = $stmt->fetch(PDO::FETCH_ASSOC);
+  }
  ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -57,31 +87,26 @@ if (isset($_POST['first_name']) && isset($_POST['last_name']) &&
     echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
     unset($_SESSION['success']);
   }
+  if ( isset($_SESSION['error2']) ) {
+      echo('<p style="color: red;">'.htmlentities($_SESSION['error2'])."</p>\n");
+    unset($_SESSION['success']);
+  }
 ?>
 <h1>Add a member</h1>
 <form method="post" enctype="multipart/form-data">
 <p>First Name:
-<input type="text" onchange="tempSession();" name="first_name" size="60"/></p>
+<input type="text" name="first_name" size="60"/></p>
 <p>Last Name:
-<input type="text" onchange="tempSession();" name="last_name" size="60"/></p>
+<input type="text" name="last_name" size="60"/></p>
 <p>Department:<br/>
-<input type="text" onchange="tempSession();" name="department" size="80"/></p>
-<!-- </form> -->
+<input type="text" name="department" size="80"/></p>
 <!-- Upload photo -->
-
-    Select image to upload (if applicable):
-    <input type="file" name="fileToUpload" id="fileToUpload">
-
+<p> Upload a profile picture (if available):
+<input type="file" name="fileToUpload" id="fileToUpload"></p>
 <p>
 <input type="submit" value="Add">
 <input type="submit" name="cancel" value="Cancel">
 </p>
-
-<script>
-  function tempSession() {
-    var x = document.getElementById(this).value;
-  }
-</script>
 
 </form>
 </body>
